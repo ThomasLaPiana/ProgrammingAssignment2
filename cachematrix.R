8fdb642ebbe8d8c98508bf271288a17fd89ec9b7
thomas.lapiana <tal103020@icloud.com>
2016-04-05 14:45:25 -0500

    Wrote the two requried assignment functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+    m=NULL
+    
+    set=function(y){
+      x<<-y
+      m<<-NULL}
+    
+    get = function() {return(x)}
+    setInv = function(i) {m<<-i}
+    getInv = function() {return(m)}
+    
+    out = list(
+      set=set,get=get,
+      setInv=setInv,getInv=getInv)
+    
+    return(out)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  
+    inv=x$getInv()
+    
+    if(!is.null(inv)) {return(inv)}
+    
+    data=x$get()
+    inv=solve(data,...)
+    x$setInv(inv)
+    return(inv)
 }
